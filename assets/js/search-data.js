76d6a791d0fd18ca06a012f173c2a73856ff9aae
BenjaRoh <benjamin-rohr@gmx.net>
2025-01-30 16:24:28 +0000

    Deploying to gh-pages from @ BenjaRoh/BenjaRoh.github.io@55f00556d7624396e76c6625729cc2ed7d653dfc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-home",
-    title: "Home",
+    title: "HOME",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
